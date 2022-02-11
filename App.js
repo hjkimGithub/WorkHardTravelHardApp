1735911653c583bd0be2e05347205130552ad01a
hjkimGithub <rlaguswo3408@gmail.com>
2022-02-11 18:19:02 +0900

    add delete button

--- App.js
+++ App.js
@@ -6,8 +6,10 @@ import {
   View, 
   TouchableOpacity, 
   TextInput,
+  Alert,
   ScrollView
 } from 'react-native';
+import { Fontisto } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { theme } from './color';
 
@@ -36,7 +38,7 @@ export default function App() {
     // const newToDos = Object.assign({}, toDos, {
     //   [Date.now()]: {text, work:working},
     // });
-    const newToDos = {
+  const newToDos = {
       ...toDos, 
       [Date.now()]: {text, working}, 
     };
@@ -44,7 +46,21 @@ export default function App() {
     await saveToDos(newToDos);
     setText("");
   }
-
+  const deleteToDo = (key) => {
+    Alert.alert("Delete To Do", "Are you sure?", [
+      { text: "Cancel" },
+      {
+        text: "I'm Sure",
+        style: "destructive",
+        onPress: async() => {
+          const newToDos = { ...toDos };
+          delete newToDos[key];
+          setToDos(newToDos);
+          await saveToDos(newToDos);
+        },
+      },
+    ]);
+  }; 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -67,11 +83,16 @@ export default function App() {
         style={styles.input}
       />
       <ScrollView>
-        {Object.keys(toDos).map((key) => (
+        {Object.keys(toDos).map((key) => 
+          toDos[key].working === working ? (
           <View style={styles.toDo} key={key}>
             <Text style={styles.toDoText}>{toDos[key].text}</Text>
+            <TouchableOpacity onPress={() => deleteToDo(key)}>
+              <Text> X </Text>
+            </TouchableOpacity>
           </View>
-        ))}
+          ) : null
+        )}
       </ScrollView>
     </View>
   );
@@ -107,6 +128,12 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
     paddingHorizontal: 20,
     borderRadius: 15,
+    flexDirection:"row",
+    alignItems:"center",
+    justifyContent:"space-between",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   toDoText: {
     color: "white",
