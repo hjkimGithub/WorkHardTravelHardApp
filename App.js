53759a19d375f0a1318a81e69677034b146f829c
hjkimGithub <rlaguswo3408@gmail.com>
2022-02-11 17:15:26 +0900

    make toDoApp

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TouchableOpacity, 
+  TextInput,
+  ScrollView
+} from 'react-native';
+import { theme } from './color';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+  const addToDo = () => {
+    if(text === "") {
+      return; 
+    }
+    // Save to do
+    // const newToDos = Object.assign({}, toDos, {
+    //   [Date.now()]: {text, work:working},
+    // });
+    const newToDos = {
+      ...toDos, 
+      [Date.now()]: {text, work: working}, 
+    };
+    setToDos(newToDos);
+    setText("");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text style={{...styles.btnText, color: working ? "white": theme.grey}}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{...styles.btnText, color: !working ? "white": theme.grey}}>Travel</Text>  
+        </TouchableOpacity>            
+      </View>
+      <TextInput 
+        onSubmitEditing={addToDo}
+        onChange={onChangeText}
+        returnKeyType="done"
+        keyboardType="number-pad"
+        placeholder={
+        working ? "Add ToDo!" : "Where to travel?"
+        } 
+        style={styles.input}
+      />
+      <ScrollView>
+        {Object.keys(toDos).map((key) => (
+          <View style={styles.toDo} key={key}>
+            <Text style={styles.toDoText}>{toDos[key].text}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +68,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 35,
+    fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    // marginTop: 10,
+    marginVertical: 20,
+    fontSize: 15,
+  },
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+  },
+  toDoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
   },
 });
